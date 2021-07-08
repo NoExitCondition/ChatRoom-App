f23616917bf9765541b128790bb1289c86b05a62
NoExitCondition <xschoolstuff@gmail.com>
2021-07-07 22:43:42 -0700

    html update + changed route handling

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const server = http.createServer(app);
 
 //Route handler
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
-  });
+  res.sendFile(__dirname + '/index.html');
+});
   
 server.listen(3000, () => {
     console.log('listening on *:3000');
